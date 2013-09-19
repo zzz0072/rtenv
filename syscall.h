6c7bee3cbc7f2c4c493f6c086d9080203e4ce4e8
Wen.Liao <zzz00072@gmail.com>
2013-09-19 11:27:55 +0800

    Move syscall defines from kernel.c to syscall.h

--- syscall.h
+++ syscall.h
@@ -2,6 +2,18 @@
 #define SYSCALL_H_20130919
 #include <stddef.h>
 
+/* System calls */
+#define SYS_CALL_FORK        (0x01)
+#define SYS_CALL_GETPID      (0x02)
+#define SYS_CALL_WRITE       (0x03)
+#define SYS_CALL_READ        (0x04)
+#define SYS_CALL_WAIT_INTR   (0x05)
+#define SYS_CALL_GETPRIORITY (0x06)
+#define SYS_CALL_SETPRIORITY (0x07)
+#define SYS_CALL_MK_NODE     (0x08)
+#define SYS_CALL_SLEEP       (0x09)
+
+/* Prototypes */
 void *activate(void *stack);
 
 int fork();
