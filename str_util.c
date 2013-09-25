8f68a952b65b38c482a30878678825bf272e017e
Wen.Liao <zzz00072@gmail.com>
2013-09-25 11:35:52 +0800

    Supoort converting integer to hex or dec string

--- str_util.c
+++ str_util.c
@@ -16,24 +16,28 @@ int strncmp(const char *str_a, const char *str_b, size_t n)
     return 0;
 }
 
-char* itoa(int val)
+#define itoa(val) itoa_base(val, 10)
+#define htoa(val) itoa_base(val, 16)
+
+static char* itoa_base(int val, int base)
 {
     static char buf[32] = { 0 };
     char has_minus = 0;
     int i = 30;
 
+    /* Sepecial case: 0 */
     if (val == 0) {
         buf[1] = '0';
         return &buf[1];
     }
 
     if (val < 0) {
-        val = val * -1;
+        val = -val;
         has_minus = 1;
     }
 
-    for (; val && (i - 1) ; --i, val /= 10)
-        buf[i] = "0123456789"[val % 10];
+    for (; val && (i - 1) ; --i, val /= base)
+        buf[i] = "0123456789abcdef"[val % base];
 
     if (has_minus) {
         buf[i] = '-';
