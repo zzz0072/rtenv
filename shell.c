67d839b460b801c310ea652635bcc24445ee831a
Wen.Liao <zzz00072@gmail.com>
2013-10-06 22:44:18 +0800

    Run host command via semihost

--- shell.c
+++ shell.c
@@ -10,101 +10,6 @@
 
 extern struct task_info g_task_info;
 
-static void cmd_ps(void)
-{
-    int i = 0;
-
-    my_printf("\rList process\n");
-
-    /* This should not happen actually */
-    if (!g_task_info.tasks) {
-        return;
-    }
-
-    /* Start list */
-    for (i = 0; i < TASK_LIMIT; i++) {
-        if(g_task_info.tasks[i].status != TASK_IS_EMPTY) {
-            my_printf("\rTID: %d\tPriority: %d\tStatus: %s\t%s\n",
-            g_task_info.tasks[i].tid,
-            g_task_info.tasks[i].priority,
-            get_task_status(g_task_info.tasks[i].status),
-            g_task_info.tasks[i].name);
-        }
-    }
-}
-
-typedef void (*cmd_func_t)(void);
-struct cmd_t
-{
-    char *name;
-    char *desc;
-    cmd_func_t handler;
-};
-static void test_exit(void);
-static void help_menu(void);
-typedef struct cmd_t cmd_entry;
-static cmd_entry available_cmds[] = {
-        {
-            .name = "ps",
-            .desc = "List process",
-            .handler = cmd_ps
-        },
-        {
-            .name = "help",
-            .desc = "This menu",
-            .handler = help_menu
-        },
-        {
-            .name = "t_exit",
-            .desc = "Test exit",
-            .handler = test_exit
-        }
-};
-
-static void exit_test_task(void)
-{
-    my_printf("\rtest...sleep 5 second\n");
-    sleep(3000); /* ms */
-    my_printf("\rAbout to exit\n");
-    exit(0);
-}
-
-static void test_exit(void) 
-{
-    if (!fork("exit_test_task")) {
-        setpriority(0, PRIORITY_DEFAULT - 10);
-        exit_test_task();
-    }
-}
-
-static void help_menu(void)
-{
-    int i = 0;
-
-    my_printf("\rAvailable Commands:\n");
-    for (i = 0; i < sizeof(available_cmds)/sizeof(cmd_entry); i++) {
-        my_printf("\r%s\t\t%s\n", available_cmds[i].name, available_cmds[i].desc);
-    }
-}
-
-static void proc_cmd(char *cmd)
-{
-    int i = 0;
-
-    /* Lets process command */
-    my_printf("\n");
-    for (i = 0; i < sizeof(available_cmds)/sizeof(cmd_entry); i++) {
-        if (strncmp(cmd, available_cmds[i].name, strlen(available_cmds[i].name)) == 0) {
-            /* Avoid subset case -> valid cmd: "ps" vs user input: "ps1" */
-            if (cmd[strlen(available_cmds[i].name)] != '\n' ) {
-                continue;
-            }
-            available_cmds[i].handler();
-            return;
-        }
-    }
-    my_printf("\rCommand not found.\n");
-}
 
 static void read_token(char *token, int max_token_chars)
 {
@@ -171,6 +76,122 @@ static void read_token(char *token, int max_token_chars)
             }
         }
     }
+}
+
+static void cmd_ps(void)
+{
+    int i = 0;
+
+    my_printf("\rList process\n");
+
+    /* This should not happen actually */
+    if (!g_task_info.tasks) {
+        return;
+    }
+
+    /* Start list */
+    for (i = 0; i < TASK_LIMIT; i++) {
+        if(g_task_info.tasks[i].status != TASK_IS_EMPTY) {
+            my_printf("\rTID: %d\tPriority: %d\tStatus: %s\t%s\n",
+            g_task_info.tasks[i].tid,
+            g_task_info.tasks[i].priority,
+            get_task_status(g_task_info.tasks[i].status),
+            g_task_info.tasks[i].name);
+        }
+    }
+}
+
+typedef void (*cmd_func_t)(void);
+struct cmd_t
+{
+    char *name;
+    char *desc;
+    cmd_func_t handler;
+};
+
+static void test_exit(void);
+static void help_menu(void);
+static void system(void);
+
+typedef struct cmd_t cmd_entry;
+static cmd_entry available_cmds[] = {
+        {
+            .name = "ps",
+            .desc = "List process",
+            .handler = cmd_ps
+        },
+        {
+            .name = "help",
+            .desc = "This menu",
+            .handler = help_menu
+        },
+        {
+            .name = "system",
+            .desc = "system\n\r\t\tRun host command",
+            .handler = system
+        },
+        {
+            .name = "t_exit",
+            .desc = "Test exit",
+            .handler = test_exit
+        }
+};
+
+static void system(void)
+{
+    char host_cmd[MAX_MSG_CHARS];
+
+    my_printf("\n\rEnter host command: ");
+    read_token(host_cmd, MAX_MSG_CHARS);
+
+    if (strlen(host_cmd) < MAX_MSG_CHARS - 1 && host_cmd[0] != '\n') {
+        host_system(host_cmd, strlen(host_cmd));
+    }
+}
+
+static void exit_test_task(void)
+{
+    my_printf("\rtest...sleep 3 second\n");
+    sleep(3000); /* ms */
+    my_printf("\rAbout to exit\n");
+    exit(0);
+}
+
+static void test_exit(void) 
+{
+    if (!fork("exit_test_task")) {
+        setpriority(0, PRIORITY_DEFAULT - 10);
+        exit_test_task();
+    }
+}
+
+static void help_menu(void)
+{
+    int i = 0;
+
+    my_printf("\rAvailable Commands:\n");
+    for (i = 0; i < sizeof(available_cmds)/sizeof(cmd_entry); i++) {
+        my_printf("\r%s\t\t%s\n", available_cmds[i].name, available_cmds[i].desc);
+    }
+}
+
+static void proc_cmd(char *cmd)
+{
+    int i = 0;
+
+    /* Lets process command */
+    my_printf("\n");
+    for (i = 0; i < sizeof(available_cmds)/sizeof(cmd_entry); i++) {
+        if (strncmp(cmd, available_cmds[i].name, strlen(available_cmds[i].name)) == 0) {
+            /* Avoid subset case -> valid cmd: "ps" vs user input: "ps1" */
+            if (cmd[strlen(available_cmds[i].name)] != '\n' ) {
+                continue;
+            }
+            available_cmds[i].handler();
+            return;
+        }
+    }
+    my_printf("\rCommand not found.\n");
 }
 
 void shell_task()
