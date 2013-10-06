4d24f810ea1431891232c191502066976fa7f61f
Wen.Liao <zzz00072@gmail.com>
2013-10-06 12:40:56 +0800

    Reorder prototype in syscall.h

--- syscall.h
+++ syscall.h
@@ -4,25 +4,21 @@
 #include <syscall_def.h>
 extern void *memcpy(void *dest, const void *src, size_t n);
 /* Prototypes */
-void *activate(void *stack);
-
-int fork(const void *proc_descption);
-int getpid();
-
 int write(int fd, const void *buf, size_t count);
 int read(int fd, void *buf, size_t count);
-
-void interrupt_wait(int intr);
 
 int getpriority(int who);
 int setpriority(int who, int value);
 
-int mknod(int fd, int mode, int dev);
-
-void sleep(unsigned int);
-
 int set_task_name(const void *proc_name, size_t count);
 int get_task_name(void *proc_name, size_t count);
+
+void *activate(void *stack);
+void sleep(unsigned int);
+void interrupt_wait(int intr);
+int  fork(const void *proc_descption);
+int  getpid();
+int  mknod(int fd, int mode, int dev);
 
 #ifdef USE_SEMIHOST
 int host_call(enum HOST_SYSCALL action, void *params);
